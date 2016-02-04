9440add5a7b325182cc1ec3934a7e72df038a5a0
Michael Schmid <michael@schmidigital.de>
2016-02-04 18:19:33 +0100

    update config

--- permissions.sh
+++ permissions.sh
@@ -3,7 +3,7 @@ set -e
 
 # Script to workaround docker-machine/boot2docker OSX host volume issues: https://github.com/docker-library/mysql/issues/99
 
-echo '* Working around permission errors locally by making sure that "mysql" uses the same uid and gid as the host volume'
+echo '** Working around permission errors locally by making sure that "mysql" uses the same uid and gid as the host volume'
 TARGET_UID=$(stat -c "%u" $FOLDER)
 echo '-- Setting mysql user to use uid '$TARGET_UID
 usermod -o -u $TARGET_UID $USER || true
